6dda92af8dddcb1687f3b23bfe4521be7f3cbb9c
John Truong <viajohnt@gmail.com>
2023-04-12 21:26:19 -0600

    Final Commit

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,16 @@
 // Code Keypad Component Here
+import React from 'react';
+
+
+
 
 function Keypad (){
+
+    function handleChange() {
+        console.log("Entering password...")
+    }
     return (
-        <div></div>
+        <input type = "password" onChange = {handleChange}/>
     )
 }
 
